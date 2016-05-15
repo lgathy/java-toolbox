252e871156468ff714cbc32e4d54adfc167fded1
lgathy <lajos.gathy@doctusoft.com>
2016-05-15 12:44:17 +0200

    Merged Outcome & Promise from ARA

---
+++ src/main/java/com/doctusoft/dataops/Outcome.java
@@ -0,0 +1,102 @@
+package com.doctusoft.dataops;
+
+import java.util.function.*;
+
+import static com.doctusoft.java.Failsafe.checkState;
+import static java.util.Objects.*;
+
+public final class Outcome<R, F> {
+    
+    public static <R, F> Outcome<R, F> resultOf(R result) {
+        return new Outcome<>(result, null);
+    }
+    
+    public static <R, F> Outcome<R, F> failure(F failure) {
+        return new Outcome<R, F>(null, requireNonNull(failure));
+    }
+    
+    private final R result;
+    
+    private final F failure;
+    
+    private Outcome(R result, F failure) {
+        this.result = result;
+        this.failure = failure;
+    }
+    
+    public boolean hasResult() {
+        return failure == null;
+    }
+    
+    public R getResult() {
+        checkState(hasResult(), () -> "Unexpected failure: " + failure);
+        return result;
+    }
+    
+    public boolean isFailure() {
+        return failure != null;
+    }
+    
+    public F getFailure() {
+        checkState(isFailure(), () -> "Unexpected result: " + result);
+        return failure;
+    }
+    
+    public Promise<R, F> toPromise() {
+        Promise<R, F> promise = new Promise<>();
+        promise.accept(this);
+        return promise;
+    }
+    
+    public String toString() {
+        if (failure == null) {
+            return "Outcome{result=" + result + "}";
+        } else {
+            return "Outcome{failure=" + failure + "}";
+        }
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static <T> Function<Outcome<T, ?>, T> toResult() {
+        return (Function) Transform.TO_RESULT;
+    }
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static <T> Function<Outcome<?, T>, T> toFailure() {
+        return (Function) Transform.TO_FAILURE;
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public enum Transform implements Function<Outcome, Object> {
+        
+        TO_RESULT {
+            public Object apply(Outcome outcome) {
+                return outcome.result;
+            }
+        },
+        TO_FAILURE {
+            public Object apply(Outcome outcome) {
+                return outcome.failure;
+            }
+        };
+        
+        public abstract Object apply(Outcome outcome);
+    }
+    
+    public enum Filter implements Predicate<Outcome<?, ?>> {
+        
+        HAS_RESULT {
+            public boolean test(Outcome<?, ?> outcome) {
+                return outcome != null && outcome.hasResult();
+            }
+        },
+        IS_FAILURE {
+            public boolean test(Outcome<?, ?> outcome) {
+                return outcome != null && outcome.isFailure();
+            }
+        };
+        
+        public abstract boolean test(Outcome<?, ?> outcome);
+    }
+    
+}
---
+++ src/main/java/com/doctusoft/dataops/Promise.java
@@ -0,0 +1,132 @@
+package com.doctusoft.dataops;
+
+import java.util.*;
+import java.util.function.*;
+
+import static com.doctusoft.java.Failsafe.checkState;
+import static java.util.Objects.*;
+
+public final class Promise<R, F> {
+    
+    private boolean finished;
+    
+    private R result;
+    
+    private F failure;
+    
+    private final LinkedList<Consumer<R>> resultConsumers = new LinkedList<>();
+    
+    private final LinkedList<Consumer<F>> failureConsumers = new LinkedList<>();
+    
+    public boolean isFinished() {
+        return finished;
+    }
+    
+    public boolean isPending() {
+        return !finished;
+    }
+    
+    public boolean hasResult() {
+        return finished && failure == null;
+    }
+    
+    public R getResult() {
+        checkState(hasResult(), () -> "Unexpected failure: " + failure);
+        return result;
+    }
+    
+    public boolean isFailed() {
+        return finished && failure != null;
+    }
+    
+    public F getFailure() {
+        checkState(isFailed(), () -> "Unexpected result: " + result);
+        return failure;
+    }
+    
+    public Promise<R, F> then(Consumer<R> consumer) {
+        requireNonNull(consumer);
+        if (hasResult()) {
+            consumer.accept(result);
+        } else if (!finished) {
+            resultConsumers.add(consumer);
+        }
+        return this;
+    }
+    
+    public Promise<R, F> fail(Consumer<F> handler) {
+        requireNonNull(handler);
+        if (isFailed()) {
+            handler.accept(failure);
+        } else if (!finished) {
+            failureConsumers.add(handler);
+        }
+        return this;
+    }
+    
+    public void resolve(R result) {
+        checkState(!finished, "promise already finished");
+        setResult(result);
+    }
+    
+    private void setResult(R result) {
+        finished = true;
+        this.result = result;
+        for (Consumer<R> consumer : resultConsumers) {
+            consumer.accept(result);
+        }
+    }
+    
+    public void reject(F failure) {
+        requireNonNull(failure, "failure");
+        checkState(!finished, "promise already finished");
+        setFailure(failure);
+    }
+    
+    private void setFailure(F failure) {
+        finished = true;
+        this.failure = failure;
+        for (Consumer<F> handler : failureConsumers) {
+            handler.accept(failure);
+        }
+    }
+    
+    public void accept(Outcome<? extends R, ? extends F> outcome) {
+        requireNonNull(outcome, "outcome");
+        checkState(!finished, "promise already finished");
+        if (outcome.isFailure()) {
+            setFailure(outcome.getFailure());
+        } else {
+            setResult(outcome.getResult());
+        }
+    }
+    
+    public void acceptIfPending(Outcome<? extends R, ? extends F> outcome) {
+        if (isPending()) {
+            accept(outcome);
+        }
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public enum Filter implements Predicate<Promise> {
+        
+        FINISHED {
+            public boolean test(Promise promise) {
+                return promise.isFinished();
+            }
+        },
+        HAS_RESULT {
+            public boolean test(Promise promise) {
+                return promise.hasResult();
+            }
+        },
+        FAILED {
+            public boolean test(Promise promise) {
+                return promise.isFailed();
+            }
+        };
+        
+        public abstract boolean test(Promise promise);
+    }
+
+}
